b318c945daaf32b30e7b1bca4ddc68e36be195aa
kirdemorgan <drankov.alex@yandex.ru>
2025-02-21 19:52:35 +0500

    init

---
+++ src/main/java/xyz/demorgan/projectpractice/ProjectPracticeApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.demorgan.projectpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPracticeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPracticeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/demorgan/projectpractice/ProjectPracticeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.demorgan.projectpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPracticeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
