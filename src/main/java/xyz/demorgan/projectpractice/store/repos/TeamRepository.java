f4a2ab308bd3752331c5b8d06cf1f222c093d0b8
kirdemorgan <drankov.alex@yandex.ru>
2025-02-22 16:35:00 +0500

    SMYZI-35 : сделаны мутации добавления и удаления

--- src/main/java/xyz/demorgan/projectpractice/store/repos/TeamRepository.java
+++ src/main/java/xyz/demorgan/projectpractice/store/repos/TeamRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import xyz.demorgan.projectpractice.store.entity.Team;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+    Team findByNameIgnoreCase(String name);
 }
