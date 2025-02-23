6e7cb605db401c6d303728cc0b4230025b1e5196
kirdemorgan <drankov.alex@yandex.ru>
2025-02-23 17:20:04 +0500

    cors fixes

--- src/main/java/xyz/demorgan/projectpractice/config/SecurityConfig.java
+++ src/main/java/xyz/demorgan/projectpractice/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
