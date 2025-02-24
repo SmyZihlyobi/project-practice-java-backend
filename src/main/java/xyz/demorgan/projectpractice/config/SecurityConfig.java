35b95d246e0f94ad2b026efaee19af0ff4313127
kirdemorgan <drankov.alex@yandex.ru>
2025-02-24 21:30:16 +0500

    method security enabled

--- src/main/java/xyz/demorgan/projectpractice/config/SecurityConfig.java
+++ src/main/java/xyz/demorgan/projectpractice/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +26,7 @@ import xyz.demorgan.projectpractice.config.jwt.UserService;
 @EnableWebSecurity
 @Configuration
 @AllArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
     UserService userService;
     JwtFilter jwtFilter;
